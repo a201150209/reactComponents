345b9baecb45c28eef7215d8ded194c823ad258e
a201150209 <48364387+a201150209@users.noreply.github.com>
2020-12-13 21:05:55 +0300

    Update correctTimer.jsx

--- correctTimer.jsx
+++ correctTimer.jsx
@@ -2,9 +2,12 @@ import React, {useState, useEffect} from 'react';
 
 const format = (value) => value <= 9 ? `0${value}` : value;
 
+//todo если экзамен завершается в свернутом состояниии, то при переходе к нему функция onFinish вызывывается дважды
+
 const Timer = (
     {
         startDate = new Date(),
+        isFinished = null,
         className = `timer`,
         runningClassName = `timerRunning`,
         stoppedClassName = `timerStopped`,
@@ -12,44 +15,54 @@ const Timer = (
         startMin = 0,
         startSec = 0,
         stopMin = 0,
-        stopSec = 0
+        stopSec = 0,
+        onFinish = () => {
+            console.log(`Время вышло`)
+        }
     }
 ) => {
     const [date, setDate] = useState(new Date(0, 0, 0, 0, startMin, startSec));
-
+    const currentDate = new Date();
     const startDateNum = Number(startDate);
-    const currentDateNum = Number(new Date());
+    const currentDateNum = Number(currentDate);
     const remainder = (currentDateNum - Number(startDate)) % 60000;
     const currentMin = Math.floor((currentDateNum - startDateNum) / 60000);
     const currentSec = Math.floor(remainder / 1000);
     const diffMin = Math.abs(startMin - stopMin);
     const diffSec = Math.abs(startSec - stopSec);
-
-
-    const isTimeOver = () => (startDateNum + diffMin * 60000 + diffSec * 1000) <= currentDateNum;
+    const stopDate = new Date(0, 0, 0, 0, stopMin, stopSec);
+    const isTimeOver = (startDateNum + diffMin * 60000 + diffSec * 1000) <= currentDateNum;
 
     useEffect(() => {
-        const timer = setInterval(() => {
-            setDate(() => {
-                if (isTimeOver()) {
-                    clearInterval(timer);
-                    return new Date(0, 0, 0, 0, stopMin, stopSec)
+            const timer = setInterval(() => {
+                setDate(() => {
+
+                    if (isDecrease) {
+                        return new Date(0, 0, 0, 0, startMin - currentMin, startSec - currentSec - 1);
+                    }
+
+                    return new Date(0, 0, 0, 0, currentMin, currentSec + 1);
+                });
+            }, 1000);
+
+            if (isTimeOver) {
+                clearInterval(timer);
+
+                if (Number(date) !== Number(stopDate)) {
+                    setDate(stopDate);
+                }
+                if (!isFinished) {
+                    setTimeout(onFinish, 0)
                 }
 
-                if (isDecrease) {
-                    return new Date(0, 0, 0, 0, startMin - currentMin, startSec - currentSec - 1);
-                }
-
-                return new Date(0, 0, 0, 0, currentMin, currentSec + 1);
-            });
-        }, 1000);
-        return () => clearInterval(timer);
-    });
+            }
+            return () => clearInterval(timer);
+        });
 
 
     return (
         <span
-            className={`${className} ${isTimeOver() ? stoppedClassName : runningClassName}`}>{format(date.getMinutes())}:{format(date.getSeconds())} мин.</span>
+            className={`${className} ${isTimeOver ? stoppedClassName : runningClassName}`}>{format(date.getMinutes())}:{format(date.getSeconds())} мин.</span>
     );
 }
 
